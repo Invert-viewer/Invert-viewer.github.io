733d17430119bbd4a92ed3ee0dbd4339ae6d4cff
Invert-viewer <2166353912@qq.com>
2022-05-22 08:12:48 +0800

    Site updated: 2022-05-22 08:12:48

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ const getScript = function(url, callback, condition) {
 const assetUrl = function(asset, type) {
   var str = CONFIG[asset][type]
   if(str.indexOf('npm')>-1||str.indexOf('gh')>-1||str.indexOf('combine')>-1)
-    return "//fastly.jsdelivr.net/" + str
+    return str
 
   if(str.indexOf('http')>-1)
     return str
